6ca200230beb2c893aa4fa348ae9ee1cfd70b39e
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-09-15 00:12:06 +0100

    mobile for homw optimised

--- movieApp/src/Header/Nav.jsx
+++ movieApp/src/Header/Nav.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/icons/Logo.png'
-import {FaEquals, FaSearch} from "react-icons/fa"
+import {FaEquals, FaSearch, FaTimes, FaBars} from "react-icons/fa"
 import './nav.scss'
 
 export default function nav() {
+  const [showNav, setShowNav] = useState(true)
+  const handleToggleNav = () => {
+    setShowNav(!showNav);
+  };
+
   return (
-    <div className='navContainer'>
+    <>
+    
+     <div className='navContainer'>
       <section className='logoSection'>
         <img src={Logo} alt="Logo" />
       </section>
@@ -13,12 +20,20 @@ export default function nav() {
         <input type="text" name="search" id="" placeholder='What do you want to watch?'/>
         <span><FaSearch/></span>
       </section>
-      <section className='menuSection'>
+      {showNav &&
+        <section className='menuSection'>
         <span>Sign in</span>
         <FaEquals 
         className='img'
         />
       </section>
-    </div>
+      }
+      
+      <div className='mobile' onClick={handleToggleNav}>
+          {showNav ? <FaTimes className='response'/> : <FaBars className='response'/>}
+        </div>
+      </div> 
+      
+    </>
   )
 }
